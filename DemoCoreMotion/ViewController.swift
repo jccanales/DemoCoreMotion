159854620bea74800a57ee00444b9b841840d563
Jean Carlo <jeancarlo@Jeans-Mac.local>
2016-01-16 13:10:32 -0500

    Initial Commit

---
+++ DemoCoreMotion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoCoreMotion
+//
+//  Created by Jean Carlo on 1/16/16.
+//  Copyright (c) 2016 UPC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
